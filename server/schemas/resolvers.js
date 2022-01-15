df94c54f496ea3caeb9d78ab88e6914b2a80c247
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-01-15 16:08:57 -0600

    back end up

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+ },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    
+    saveBook: async (parents, args, context ) => {
+        if (!context.user) {
+        throw new AuthenticationError ("Not logged in!");
+        }
+      else {
+          const updatedUser = await User.findByIdAndUpdate (
+            // finds the user 
+            {_id: context.user._id},
+            {$push: {savedBooks: args}},
+            {new:true}
+
+          );
+          return updatedUser;
+      }},
+
+           removeBook: async (parents, {bookId}, context ) => {
+        if (!context.user) {
+        throw new AuthenticationError ("Not logged in!");
+        }
+      else {
+          const updatedUser = await User.findByIdAndUpdate (
+            // finds the user 
+            {_id: context.user._id},
+            {$pull: {savedBooks: {bookId}}},
+            {new:true}
+
+          );
+          return updatedUser;
+      }}
+    
+    
+    }
+
+
+};
+
+module.exports = resolvers;
